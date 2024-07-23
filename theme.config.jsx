38dc55b368438a58e1f98111449f86e5bd6a9c10
yathi27 <70001896+yathi27@users.noreply.github.com>
2024-07-23 11:16:00 +0200

    Add files via upload

---
+++ theme.config.jsx
@@ -0,0 +1,10 @@
+export default {
+    logo: <span>Rockwell Automation</span>,
+    project: {
+      link: 'https://github.com/shuding/nextra'
+    },
+    footer: {
+        text: ""
+    }
+    // ... other theme options
+  }
